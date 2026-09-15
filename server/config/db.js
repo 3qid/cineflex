0f3a1a72d84b4660dcb371cd748b7f42ba1a1a3e
hamdan <hamdanalsbahy@gmail.com>
2026-09-15 19:46:56 +0400

    Support DB_SSL env for hosted MySQL (Aiven free requires SSL)

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,10 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
+  dialectOptions:
+    process.env.DB_SSL === 'true'
+      ? { ssl: { rejectUnauthorized: false } }
+      : undefined,
 });
 
 async function testDbConnection() {
